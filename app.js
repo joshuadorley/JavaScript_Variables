17b7cc4391007123356becf24dadb5bfea6e1fe6
joshuadorley <joshuaboateng33@gmail.com>
2024-09-04 18:05:32 -0400

    Final

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Frodo";
+let lastName = "Baggins";
+var age = 51;
 
-// Exercise 2
+lastName = "Gamgee"
+age = 52;
 
-// Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
